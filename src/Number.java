c8ca09604608e69895b05febf67dbde2723c547f
ninipun <saakshi.bhandari11@gmail.com>
2020-04-22 17:06:30 +0530

    adding odd even file and removing unnecessary files

---
+++ src/Number.java
@@ -0,0 +1,11 @@
+public class Number {
+
+    public void calcuteEvenNumber(int number) {
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+
+    public static final String name = "Sakshi";
+
+    public static void printHello() {
+        System.out.println("Hello");
+
+
+    }
+
+    public void hello() {
+        System.out.println("gadjsgjsad");
+    }
+
+
+}
+
